50437982f22c8b994e074e5662d08a688229f2a9
Nikita <nikitapashinsky@gmail.com>
2023-02-02 00:04:18 +0300

    fix borders

--- src/components/GuessRow.jsx
+++ src/components/GuessRow.jsx
@@ -9,14 +9,20 @@ export default function GuessRow({ guess, answer }) {
       <div
         className={`
         ${
-          status === "misplaced" &&
-          "dark:border-yellow-400/30 dark:bg-yellow-400/50 dark:text-yellow-100"
+          status === "misplaced"
+            ? "dark:border-yellow-400/30 dark:bg-yellow-400/50 dark:text-yellow-100"
+            : ""
         } 
         ${
-          status === "correct" &&
-          "dark:border-green-300/25 dark:bg-green-400/50 dark:text-green-100"
+          status === "correct"
+            ? "dark:border-green-300/25 dark:bg-green-400/50 dark:text-green-100"
+            : ""
         }
-        ${status === "incorrect" && "dark:bg-neutral-800 dark:text-neutral-400"}
+        ${
+          status === "incorrect"
+            ? "dark:bg-neutral-800 dark:text-neutral-400"
+            : ""
+        }
         flex aspect-square w-14 items-center justify-center rounded-2xl border-2 border-neutral-200 text-center font-bold dark:border-neutral-700/50`}
       >
         <span className="">{letter}</span>
